cffaa0f82724bb7ba379d5ea74e0accbe9979a5e
dee-sarania <dhritideepasarania@gmail.com>
2023-11-03 15:05:45 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <pre>{{ pages }}</pre>
+      
+    </div>
+</template>
+
+<script setup>
+
+const pages= ref();
+
+onMounted(async() => {
+const {result} = await $fetch("/api/query?col=pages");
+console.log(result)
+pages.value= result;
+});
+</script>
+
+<style scoped></style>
